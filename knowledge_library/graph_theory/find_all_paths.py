f565b726520c00e12c97627e195c2adf1c205de0
Kimathi <kiprotich.kaai@gmail.com>
2019-04-07 01:25:01 -0400

    Including what you should expect the output to be when running the find_all_paths module

--- knowledge_library/graph_theory/find_all_paths.py
+++ knowledge_library/graph_theory/find_all_paths.py
@@ -29,3 +29,18 @@ print(path)
 print('All paths from vertex "c" to vertex "c":')
 path = graph.find_all_paths("c", "c")
 print(path)
+
+'''
+EXPECTED OUTPUT:
+
+Vertices of graph:
+['a', 'b', 'c', 'd', 'e', 'f']
+Edges of graph:
+[{'a', 'd'}, {'f', 'a'}, {'c', 'b'}, {'c'}, {'c', 'd'}, {'e', 'c'}, {'f', 'd'}]
+All paths from vertex "a" to vertex "b":
+[['a', 'd', 'c', 'b'], ['a', 'f', 'd', 'c', 'b']]
+All paths from vertex "a" to vertex "f":
+[['a', 'f']]
+All paths from vertex "c" to vertex "c":
+[['c']]
+'''
